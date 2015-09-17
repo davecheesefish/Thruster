b9615e7072f8d2046ecfb657f9f8379fe691f807
David Prior <david.prior@davecheesefish.com>
2015-09-17 13:05:11 +0100

    Added functions to Vector2d to calculate direction and magnitude.

--- src/math/vector2d.js
+++ src/math/vector2d.js
@@ -5,6 +5,22 @@ define(function(){
 		this.y = y;
 	};
 	
+	/**
+	 * Calculates the length of this vector.
+	 * @returns The length of the vector.
+	 */
+	Vector2d.prototype.length = function(){
+		return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
+	};
+	
+	/**
+	 * Calculates the angle of this vector from the positive x axis.
+	 * @returns The angle of this vector from the positive x axis.
+	 */
+	Vector2d.prototype.angle = function(){
+		return Math.atan2(this.y, this.x);
+	}
+	
 	return Vector2d;
 	
 });
--- tests/vectors.js
+++ tests/vectors.js
@@ -1,8 +1,11 @@
 module('Vectors');
 
 test('Vector2d', function(assert){
-	var vector = new Thruster.Math.Vector2d(1, 2);
+	var vector = new Thruster.Math.Vector2d(3, 4);
 	
-	assert.equal(vector.x, 1, 'Constructed x value is set correctly.');
-	assert.equal(vector.y, 2, 'Constructed y value is set correctly.');
+	assert.equal(vector.x, 3, 'Constructed x value is set correctly.');
+	assert.equal(vector.y, 4, 'Constructed y value is set correctly.');
+	
+	assert.equal(vector.length(), 5, 'Vector length is correctly calculated.');
+	assert.equal(vector.angle(), 0.9272952180016122, 'Vector angle is correctly calculated.')
 });
