8579745b8accd194c8e69bcbc6ca461e75543745
David Prior <david.prior@davecheesefish.com>
2015-09-20 01:01:55 +0100

    Added Shapes.Point.vectorTo().

--- src/shapes/point2d.js
+++ src/shapes/point2d.js
@@ -1,4 +1,4 @@
-define(function(){
+define(['math/vector2d'], function(Vector2d){
 	
 	/**
 	 * Class representing a point in 2-dimensional Euclidean space.
@@ -9,6 +9,15 @@ define(function(){
 	var Point2d = function(x, y){
 		this.x = x;
 		this.y = y;
+	};
+	
+	/**
+	 * Get the vector from this point to the other point provided.
+	 * @param {Thruster.Shapes.Point} point The target point.
+	 * @returns {Thruster.Math.Vector2d}
+	 */
+	Point2d.prototype.vectorTo = function(point){
+		return new Vector2d(point.x - this.x, point.y - this.y);
 	};
 	
 	return Point2d;
--- tests/shapes/point2d.js
+++ tests/shapes/point2d.js
@@ -1,8 +1,18 @@
-QUnit.module('Thruster.Shapes.Point');
+QUnit.module('Thruster.Shapes.Point2d');
 
 QUnit.test('Constructor', function(assert){
 	var point = new Thruster.Shapes.Point2d(1, 2);
 	
 	assert.equal(point.x, 1, 'x property set correctly.');
 	assert.equal(point.y, 2, 'y property set correctly.');
+});
+
+QUnit.test('vectorTo()', function(assert){
+	// Find the vector from point1 to point2.
+	var point1 = new Thruster.Shapes.Point2d(1, 2),
+		point2 = new Thruster.Shapes.Point2d(3, 5);
+	
+	var vector = point1.vectorTo(point2);
+	
+	assert.propEqual(vector, new Thruster.Math.Vector2d(2, 3), 'Returned value is correct.');
 });
