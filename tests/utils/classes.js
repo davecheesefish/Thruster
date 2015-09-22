dffd92c63050196b3f4258d01d27c082b0a8b4d2
David Prior <david.prior@davecheesefish.com>
2015-09-22 19:15:37 +0100

    Updated unit test for Thruster.Utils.Classes.extend().

--- tests/utils/classes.js
+++ tests/utils/classes.js
@@ -3,26 +3,43 @@ QUnit.module('Thruster.Utils.Classes');
 QUnit.test('extend()', function(assert){
 	// Child should inherit all properties of Parent.
 	var Parent = function(){
-		this.localProp = true;
-		this.localFunc = function(){
+		this.parentProp = true;
+		this.parentFunc = function(){
 			return true;
 		};
 	};
 	
-	Parent.prototype.prototypeProp = true;
-	
-	Parent.prototype.prototypeFunc = function(){
+	Parent.prototype.parentProtoProp = true;
+	Parent.prototype.parentProtoFunc = function(){
 		return true;
 	};
+	Parent.prototype.overridenFunction = function(){
+		return 'Parent';
+	};
 	
-	var Child = function(){};
+	
+	var Child = function(){
+		this.childProp = true;
+		this.childFunc = function(){
+			return true;
+		};
+	};
+	
 	Thruster.Utils.Classes.extend(Parent, Child);
+	
+	Child.prototype.overridenFunction = function(){
+		return 'Child';
+	};
 	
 	// Create an instance of Child
 	var childInstance = new Child();
 	
-	assert.ok(childInstance.localProp, 'Child class inherits properties from the parent.');
-	assert.ok(childInstance.localFunc(), 'Child class inherits functions from the parent.');
-	assert.ok(childInstance.prototypeProp, 'Child class inherits properties from the parent\'s prototype.');
-	assert.ok(childInstance.prototypeFunc(), 'Child class inherits functions from the parent\'s prototype.');
+	assert.ok(childInstance.parentProp, 'Child class inherits properties from the parent.');
+	assert.ok(childInstance.parentFunc(), 'Child class inherits functions from the parent.');
+	assert.ok(childInstance.parentProtoProp, 'Child class inherits properties from the parent\'s prototype.');
+	assert.ok(childInstance.parentProtoFunc(), 'Child class inherits functions from the parent\'s prototype.');
+	
+	assert.ok(childInstance.childProp, 'Child class properties are preserved.');
+	assert.ok(childInstance.childFunc(), 'Child class functions are preserved.');
+	assert.equal(childInstance.overridenFunction(), 'Child', 'Child functions override parent functions of the same name.');
 });
