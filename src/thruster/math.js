52ab1c235b8a7dd68bc8c603e720ab7ab85420c3
David Prior <david.prior@davecheesefish.com>
2015-10-11 19:34:38 +0100

    Added thruster.math.numbers namespace. Added thruster.math.numbers.lerp().

--- src/thruster/math.js
+++ src/thruster/math.js
@@ -1,10 +1,12 @@
-define(['thruster/math/vector2d'], /** @lends math */ function(Vector2d){
+define(['thruster/math/numbers', 'thruster/math/vector2d'], /** @lends math */ function(numbers, Vector2d){
 	
 	/**
 	 * @namespace
 	 * @memberof thruster
 	 */
 	var math = {
+		numbers: numbers,
+		
 		Vector2d: Vector2d
 	};
 	
---
+++ tests/unit/math/numbers.js
@@ -0,0 +1,11 @@
+(function(){
+	'use strict';
+	QUnit.module('thruster.math.numbers');
+	
+	QUnit.test('lerp()', function(assert){
+		assert.equal(thruster.math.numbers.lerp(1, 2, 0), 1, 'Returns the first argument if alpha is 0.');
+		assert.equal(thruster.math.numbers.lerp(1, 2, 1), 2, 'Returns the second argument if alpha is 1.');
+		assert.equal(thruster.math.numbers.lerp(1, 2, 0.5), 1.5, 'Returns halfway between the arguments if alpha is 0.5.');
+		assert.equal(thruster.math.numbers.lerp(2, 0, 0.75), 0.5, 'Returns the correct value where b is less than a.');
+	});
+})();
