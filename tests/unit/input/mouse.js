6cc529c75d98f4a3548a0bd8f8883a58c18b1c97
David Prior <david.prior@davecheesefish.com>
2015-10-12 16:36:14 +0100

    Fixed thruster.input.mouse.detach() unit test failing in Internet Explorer.

--- tests/unit/input/mouse.js
+++ tests/unit/input/mouse.js
@@ -10,7 +10,7 @@
 		
 		// Move mouse to (50, 50)
 		ev = document.createEvent('Event');
-		ev.initEvent('mousemove');
+		ev.initEvent('mousemove', false, false);
 		ev.clientX = 50;
 		ev.clientY = 50;
 		canvasEl.dispatchEvent(ev);
@@ -24,7 +24,7 @@
 		
 		// Move mouse to (20, 20)
 		ev = document.createEvent('Event');
-		ev.initEvent('mousemove');
+		ev.initEvent('mousemove', false, false);
 		ev.clientX = 20;
 		ev.clientY = 20;
 		canvasEl.dispatchEvent(ev);
